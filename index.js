a0ec1abbbb86e41240be146410ec4cf275dbccac
ivan verushkin <ivanveruskin86@gmail.com>
2024-09-16 17:35:08 +0300

    Add create new toDo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ window.onload = function () {
     console.log(array1);
     const outputElement = document.getElementById('output');
     array1.forEach(item => {
-        const listItem = document.createElement('div');
+    const listItem = document.createElement('div');
     listItem.innerText = `${item.toDo}`;
     listItem.style.border = "1px solid black";
     listItem.style.marginTop = "30px"; 
@@ -26,4 +26,38 @@ window.onload = function () {
     listItem.style.textAlign ="center";
     outputElement.appendChild(listItem);
     });
+}
+function AddNewToDo()
+{
+    const inputValue = document.getElementById('AddToDo');
+    if(inputValue.value === null){
+        return;
+    }
+    const outputElement = document.getElementById('output');
+    const listItem = document.createElement('div');
+    listItem.innerText = inputValue.value;
+    listItem.style.border = "1px solid black";
+    listItem.style.marginTop = "30px"; 
+    listItem.style.marginBottom = "30px"; 
+    listItem.style.paddingTop = "30px"; 
+    listItem.style.paddingBottom = "30px"; 
+    listItem.style.color = "white";
+    listItem.style.backgroundColor = "rgb(56, 175, 56)";
+    listItem.style.width = "600px";
+    listItem.style.textAlign ="center";
+    outputElement.appendChild(listItem);
+    var response = httpPost('https://localhost:7262/api/ToDo', inputValue.value);
+    console.log(response);
+}
+
+function httpPost(theUrl, input)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "POST", theUrl, false ); // false for synchronous request
+    xmlHttp.setRequestHeader("Content-Type", "application/json"); // Set Content-Type to JSON
+    
+    // Convert input object to JSON string
+    var jsonInput = JSON.stringify(input);
+    xmlHttp.send( jsonInput);
+    return xmlHttp.responseText;
 }
