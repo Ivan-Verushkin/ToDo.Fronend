b8d4b96c670039a68536eb5e2ecba360853fb2ec
Ophiuch <amare.anguis@gmail.com>
2024-09-17 12:28:51 +0300

    styled input and submit btn

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const url = 'https://localhost:7262/api/ToDo';
 
-function httpGet(theUrl)
-{
+function httpGet(theUrl) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
     return xmlHttp.responseText;
 }
-window.onload = function () { 
+window.onload = function () {
 
     var response = httpGet(url);
     // var response = '[{"id":1,"toDo":"Wash a car"},{"id":3,"toDo":"Watch a movie"}]';
-    var array1 = JSON.parse(response); 
+    var array1 = JSON.parse(response);
     console.log(array1);
     const outputElement = document.getElementById('output');
     array1.forEach(item => {
-    const listItem = document.createElement('div');
-    listItem.innerText = `${item.toDo}`;
-    listItem.style.border = "1px solid black";
-    listItem.style.marginTop = "60px"; 
-    listItem.style.paddingTop = "30px"; 
-    listItem.style.paddingBottom = "30px"; 
-    listItem.style.color = "white";
-    listItem.style.backgroundColor = "rgb(56, 175, 56)";
-    listItem.style.width = "600px";
-    listItem.style.textAlign ="center";
-    outputElement.appendChild(listItem);
-    AddUpdateDeleteButtons(outputElement, item.id);
+        const listItem = document.createElement('div');
+        listItem.innerText = `${item.toDo}`;
+        listItem.style.border = "1px solid black";
+        listItem.style.marginTop = "60px";
+        listItem.style.paddingTop = "30px";
+        listItem.style.paddingBottom = "30px";
+        listItem.style.color = "white";
+        listItem.style.backgroundColor = "rgb(56, 175, 56)";
+        listItem.style.width = "600px";
+        listItem.style.textAlign = "center";
+        outputElement.appendChild(listItem);
+        const inputField = document.getElementById('#AddToDo');
+        AddUpdateDeleteButtons(outputElement, item.id);
     });
 }
-function AddUpdateDeleteButtons(outputElement, id){
+function AddUpdateDeleteButtons(outputElement, id) {
     const inputElement = document.createElement('div');
     inputElement.style.width = "600px";
     inputElement.style.height = "50px";
@@ -41,7 +41,7 @@ function AddUpdateDeleteButtons(outputElement, id){
     updateButton.style.color = "white";
     inputElement.appendChild(updateButton);
     const deleteButton = document.createElement('button');
-    deleteButton.onclick = function(){ httpDelete(url, id)};
+    deleteButton.onclick = function () { httpDelete(url, id) };
     deleteButton.innerText = 'Delete';
     deleteButton.style.width = "300px";
     deleteButton.style.height = "50px";
@@ -50,61 +50,57 @@ function AddUpdateDeleteButtons(outputElement, id){
     inputElement.appendChild(deleteButton);
     outputElement.appendChild(inputElement);
 }
-function AddNewToDo()
-{
+function AddNewToDo() {
     const inputValue = document.getElementById('AddToDo');
-    if(inputValue.value === null){
+    if (inputValue.value === null) {
         return;
     }
     const outputElement = document.getElementById('output');
     const listItem = document.createElement('div');
     listItem.innerText = inputValue.value;
     listItem.style.border = "1px solid black";
-    listItem.style.marginTop = "30px"; 
-    listItem.style.marginBottom = "30px"; 
-    listItem.style.paddingTop = "30px"; 
-    listItem.style.paddingBottom = "30px"; 
+    listItem.style.marginTop = "30px";
+    listItem.style.marginBottom = "30px";
+    listItem.style.paddingTop = "30px";
+    listItem.style.paddingBottom = "30px";
     listItem.style.color = "white";
     listItem.style.backgroundColor = "rgb(56, 175, 56)";
     listItem.style.width = "600px";
-    listItem.style.textAlign ="center";
+    listItem.style.textAlign = "center";
     outputElement.appendChild(listItem);
     var response = httpPost(url, inputValue.value);
     const array2 = JSON.parse(response);
-    var position = array2[array2.length-1].id;
+    var position = array2[array2.length - 1].id;
     AddUpdateDeleteButtons(outputElement, position);
     console.log(response);
 }
 
-function httpPost(theUrl, input)
-{
+function httpPost(theUrl, input) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "POST", theUrl, false ); // false for synchronous request
+    xmlHttp.open("POST", theUrl, false); // false for synchronous request
     xmlHttp.setRequestHeader("Content-Type", "application/json"); // Set Content-Type to JSON
-    
+
     // Convert input object to JSON string
     var jsonInput = JSON.stringify(input);
-    xmlHttp.send( jsonInput);
+    xmlHttp.send(jsonInput);
     return xmlHttp.responseText;
 }
 
-function httpPut(theUrl, input)
-{
+function httpPut(theUrl, input) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "POST", theUrl, false ); // false for synchronous request
+    xmlHttp.open("POST", theUrl, false); // false for synchronous request
     xmlHttp.setRequestHeader("Content-Type", "application/json"); // Set Content-Type to JSON
-    
+
     // Convert input object to JSON string
     var jsonInput = JSON.stringify(input);
-    xmlHttp.send( jsonInput);
+    xmlHttp.send(jsonInput);
     return xmlHttp.responseText;
 }
 
-function httpDelete(theUrl, id)
-{
+function httpDelete(theUrl, id) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "DELETE", theUrl + "/" + id, false ); // false for synchronous request
-    xmlHttp.send( null );
+    xmlHttp.open("DELETE", theUrl + "/" + id, false); // false for synchronous request
+    xmlHttp.send(null);
     location.reload();
     return xmlHttp.responseText;
 }
